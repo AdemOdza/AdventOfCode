e54ebe771705451afd506d5ca9b9abd0cae5de8c
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-12-07 12:30:41 -0600

    HAhahha

--- 2021/Day2/src/main.rs
+++ 2021/Day2/src/main.rs
@@ -1,6 +1,13 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("data.txt");
-    println!("Hello, world!");
+    let contents = fs::read_to_string("data.txt").unwrap();
+    let mut split = contents.trim().split("\n");
+
+    let mut v: Vec<(&str, i32)>  = Vec::new();
+
+    for item in split {
+        let temp: Vec<&str> = item.trim().split(' ').into_iter().collect();
+        
+    }
 }
