4e417ada5e291c37d298501d452ee59f8bebab58
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-12-08 19:57:08 -0600

    Delete 2021/Day1.2/target directory

--- 2021/Day1.2/target/debug/Day2.d
+++
@@ -1,1 +0,0 @@
-/Users/adem/Desktop/AdventOfCode/AdventOfCode/2021/Day1.2/target/debug/Day2: /Users/adem/Desktop/AdventOfCode/AdventOfCode/2021/Day1.2/src/main.rs
--- 2021/Day1.2/target/rls/debug/deps/day_1_2-6fc8930f3772d8fc.d
+++
@@ -1,5 +0,0 @@
-/Users/adem/Desktop/AdventOfCode/AdventOfCode/2021/Day1.2/target/rls/debug/deps/day_1_2-6fc8930f3772d8fc.rmeta: src/main.rs
-
-/Users/adem/Desktop/AdventOfCode/AdventOfCode/2021/Day1.2/target/rls/debug/deps/day_1_2-6fc8930f3772d8fc.d: src/main.rs
-
-src/main.rs:
