b24133d911f4d0f5fa41042db5f087639aae5c04
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-12-08 19:52:45 -0600

    Finished day 2

--- 2021/Day2/src/main.rs
+++ 2021/Day2/src/main.rs
@@ -4,24 +4,29 @@ fn main() {
     let contents = fs::read_to_string("data.txt").unwrap();
     let split = contents.trim().split("\n");
 
-    let v: Vec<(&str, i32)>  = Vec::new();
+    let mut v: Vec<(&str, i32)>  = Vec::new();
 
     for item in split {
         
         let temp: Vec<&str> = item.trim().split(' ').into_iter().collect();
         let curr_val: (&str, i32) = (temp[0], temp[1].parse::<i32>().unwrap());
+        v.push(curr_val);
     }
 
-    let mut depth = 0;
-    let mut pos = 0;
+    let mut depth: i32 = 0;
+    let mut pos: i32  = 0;
+    let mut aim: i32  = 0;
 
     for command in v {
         println!("Command -> \"{} {}\"", command.0, command.1);
         match command.0{
-            "forward" => pos = pos + command.1,
+            "forward" => {
+                pos = pos + command.1;
+                depth = depth + (aim * command.1); 
+            }, 
             "backward" => pos = pos - command.1,
-            "down" => depth = depth + command.1,
-            "up" => depth = depth - command.1,
+            "down" => aim = aim + command.1,
+            "up" => aim = aim - command.1,
             _ => println!("Error: Invalid command"),
         }
     }
