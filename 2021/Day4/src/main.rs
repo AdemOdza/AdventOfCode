7ee8c2e624471909da326ba0e3ec49c75b5790cc
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-12-11 15:43:09 -0600

    vecveci32 working

--- 2021/Day4/src/main.rs
+++ 2021/Day4/src/main.rs
@@ -5,10 +5,26 @@ fn main() {
 
     let numbers: Vec<i32> = split.next().unwrap().split(',').map(|s| s.parse().unwrap()).collect();
     println!("{:?}", numbers);
+    let mut boards: Vec<Bingo> = Vec::new();
 
     for block in split {
-            let board: Vec<Vec<i32>> = block.split('\n')
-            .map(|x| x.split(' ').map(|y| y.parse::<i32>())).collect();
+            let mut board: Vec<Vec<i32>> = block
+            .split('\n')
+            .map(|x| x.split_whitespace().map(|y| y.parse::<i32>().unwrap()).collect())
+            .collect();
+
+            let mut tempBoard = Bingo {
+                content: &board,
+                found: Vec::new()
+            };
+            boards.push(tempBoard);
     }
+
+    
 }
 
+struct Bingo {
+    content: Vec<Vec<i32>>,
+    found: Vec<Vec<bool>>,
+
+}
